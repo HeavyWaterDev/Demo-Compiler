528f5ba3c08f38942f73698a633d282586a6b4e4
Joshua Pow <joshua.pow@npxinnovation.ca>
2024-09-13 15:42:31 -0400

    small changes

--- Exercise Compiler/interpreter.py
+++ Exercise Compiler/interpreter.py
@@ -1,49 +1,43 @@
 class Interpreter:
     def __init__(self):
         self.variables = {}
+        # Dictionary of binary operations
+        self.operations = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: x / y,
+            '==': lambda x, y: x == y,
+            '!=': lambda x, y: x != y,
+            '<': lambda x, y: x < y,
+            '<=': lambda x, y: x <= y,
+            '>': lambda x, y: x > y,
+            '>=': lambda x, y: x >= y,
+        }
 
     def evaluate(self, node):
+        # Handle list of statements
         if isinstance(node, list):
-            last_result = None
-            for stmt in node:
-                last_result = self.evaluate(stmt)
-            return last_result
-        elif node[0] == 'number':
+            return [self.evaluate(stmt) for stmt in node][-1]
+        
+        node_type = node[0]
+        
+        # Handle different node types
+        if node_type == 'number':
             return node[1]
-        elif node[0] == 'name':
+        elif node_type == 'name':
             return self.variables.get(node[1], 0)
-        elif node[0] == 'binop':
-            left = self.evaluate(node[2])
-            right = self.evaluate(node[3])
-            if node[1] == '+':
-                pass #TODO: Implement addition
-            elif node[1] == '-':
-                return left - right
-            elif node[1] == '*':
-                pass #TODO: Implement multiplication
-            elif node[1] == '/':
-                return left / right
-            elif node[1] == '==':
-                return left == right
-            elif node[1] == '!=':
-                return left != right
-            elif node[1] == '<':
-                pass #TODO: Implement less than
-            elif node[1] == '<=':
-                return left <= right
-            elif node[1] == '>':
-                return left > right
-            elif node[1] == '>=':
-                return left >= right
-        elif node[0] == 'assign':
-            pass #TODO: Implement assignment
-        elif node[0] == 'expr':
+        elif node_type == 'binop':
+            op, left, right = node[1], self.evaluate(node[2]), self.evaluate(node[3])
+            return self.operations[op](left, right)
+        elif node_type == 'assign':
+            self.variables[node[1]] = self.evaluate(node[2])
+        elif node_type == 'expr':
             return self.evaluate(node[1])
-        elif node[0] == 'if':
-            condition = self.evaluate(node[1])
-            if condition:
+        elif node_type == 'if':
+            if self.evaluate(node[1]):
                 return self.evaluate(node[2])
-        elif node[0] == 'loop':
+        elif node_type == 'loop':
             count = self.evaluate(node[1])
             for _ in range(count):
                 self.evaluate(node[2])
